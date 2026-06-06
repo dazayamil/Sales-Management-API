02b7faba92375924f9bf2a16a3f1d244dd4dd99a
daza yamil <dazayamil07@gmail.com>
2026-06-06 18:46:17 -0300

    refactor: migrate request and response DTOs to records

--- src/main/java/com/dazayamil/tiendabel/dto/response/PaymentBreakdownDTO.java
+++ src/main/java/com/dazayamil/tiendabel/dto/response/PaymentBreakdownDTO.java
@@ -1,15 +1,10 @@
 package com.dazayamil.tiendabel.dto.response;
 
 import com.dazayamil.tiendabel.model.enums.Payment;
-import lombok.Builder;
-import lombok.Data;
-
 import java.math.BigDecimal;
 
-@Data
-@Builder
-public class PaymentBreakdownDTO {
-    private Payment paymentMethod;
-    private int count;
-    private BigDecimal total;
-}
+public record PaymentBreakdownDTO (
+    Payment paymentMethod,
+    int count,
+    BigDecimal total
+){}
